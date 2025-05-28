921707d75967b1c990e0df8e771015d2cf3184af
Nick Black <49686365+nickblack26@users.noreply.github.com>
2025-05-28 08:53:11 -0700

    removed filter schema

--- frontend/src/routes/_authed/tickets/index.tsx
+++ frontend/src/routes/_authed/tickets/index.tsx
@@ -6,12 +6,12 @@ import { DataTable } from '@/components/ui/data-table';
 import { zodValidator } from '@tanstack/zod-adapter';
 import { getTicketsQuery } from '@/lib/manage/api';
 import { psaConfig } from '@/utils/manage/params';
-import { filterSchema } from '@/lib/utils';
+// import { filterSchema } from '@/lib/utils';
 import type { ServiceTicket } from '@/types/manage';
 
 export const Route = createFileRoute('/_authed/tickets/')({
 	component: RouteComponent,
-	validateSearch: zodValidator(filterSchema),
+	// validateSearch: zodValidator(filterSchema),
 });
 
 function RouteComponent() {
@@ -23,27 +23,27 @@ function RouteComponent() {
 		'board/id': [22, 26, 30, 31],
 	};
 
-	params?.filter?.forEach((f) => {
-		const column = columns.find((c) => c.id === f.id);
+	// params?.filter?.forEach((f) => {
+	// 	const column = columns.find((c) => c.id === f.id);
 
-		if (!column) return;
+	// 	if (!column) return;
 
-		if (column.meta?.type === 'text') {
-			// console.log((f.value.values as Array<string>).join(','));
-			conditions = Object.assign(conditions, {
-				[column.meta?.filterKey as string]: {
-					comparison: psaConfig.textOperators.find((o) => o.label.toLowerCase() === f.value.operator)?.value,
-					value: `'${(f.value.values as Array<string>).join(',') ?? ' '}'`,
-				},
-			});
-		}
+	// 	if (column.meta?.type === 'text') {
+	// 		// console.log((f.value.values as Array<string>).join(','));
+	// 		conditions = Object.assign(conditions, {
+	// 			[column.meta?.filterKey as string]: {
+	// 				comparison: psaConfig.textOperators.find((o) => o.label.toLowerCase() === f.value.operator)?.value,
+	// 				value: `'${(f.value.values as Array<string>).join(',') ?? ' '}'`,
+	// 			},
+	// 		});
+	// 	}
 
-		// if (Array.isArray(f.value.values)) {
-		// 	conditions = Object.assign(conditions, { [column.meta?.filterKey as string]: f.value.values });
-		// }
-	});
+	// 	// if (Array.isArray(f.value.values)) {
+	// 	// 	conditions = Object.assign(conditions, { [column.meta?.filterKey as string]: f.value.values });
+	// 	// }
+	// });
 
-	const columnSort = columns.find((c) => c.id === params.sort?.field);
+	// const columnSort = columns.find((c) => c.id === params.sort?.field);
 
 	return (
 		<main className='p-3 space-y-1.5'>
@@ -52,7 +52,7 @@ function RouteComponent() {
 			</section>
 
 			<Suspense fallback={<TableSkeleton columns={columns.length} />}>
-				<DataTable
+				{/* <DataTable
 					columns={columns}
 					options={getTicketsQuery({
 						conditions,
@@ -63,7 +63,7 @@ function RouteComponent() {
 							order: params.sort?.direction,
 						},
 					})}
-				/>
+				/> */}
 			</Suspense>
 		</main>
 	);
