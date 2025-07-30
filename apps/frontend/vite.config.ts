bc476224eff4b6440060429b0ecf8e8f4183f83d
Nick Black <49686365+nickblack26@users.noreply.github.com>
2025-07-30 06:57:25 -0700

    removed vite config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,22 +1,22 @@
 // vite.config.ts
 
-import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	plugins: [
 		tsconfigPaths({
-			projects: ["./tsconfig.json"],
+			projects: ['./tsconfig.json'],
 		}),
 		tanstackStart({
 			customViteReactPlugin: true,
-			target: "vercel",
-			spa: {
-				enabled: true,
-			},
+			target: 'vercel',
+			// spa: {
+			// 	enabled: true,
+			// },
 		}),
 		tailwindcss(),
 		react(),
