23e0bcd81f4894e948131f1cd38e6358052a886b
Nick Black <49686365+nickblack26@users.noreply.github.com>
2025-07-29 22:22:39 -0700

    browser

--- apps/frontend/src/lib/supabase/server.ts
+++ apps/frontend/src/lib/supabase/server.ts
@@ -1,27 +1,27 @@
-import { createServerClient } from '@supabase/ssr';
-import { parseCookies, setCookie } from '@tanstack/react-start/server';
+import { createBrowserClient } from '@supabase/ssr';
+// import { parseCookies, setCookie } from '@tanstack/react-start/server';
 import { env } from '@/lib/utils';
 
 export function getSupabaseServerClient() {
-	return createServerClient(
+	return createBrowserClient(
 		env.VITE_SUPABASE_URL,
-		env.VITE_SUPABASE_ANON_KEY,
-		{
-			cookies: {
-				getAll() {
-					return Object.entries(parseCookies()).map(
-						([name, value]) => ({
-							name,
-							value,
-						})
-					);
-				},
-				setAll(cookies) {
-					cookies.forEach((cookie) => {
-						setCookie(cookie.name, cookie.value);
-					});
-				},
-			},
-		}
+		env.VITE_SUPABASE_ANON_KEY
+		// {
+		// 	cookies: {
+		// 		getAll() {
+		// 			return Object.entries(parseCookies()).map(
+		// 				([name, value]) => ({
+		// 					name,
+		// 					value,
+		// 				})
+		// 			);
+		// 		},
+		// 		setAll(cookies) {
+		// 			cookies.forEach((cookie) => {
+		// 				setCookie(cookie.name, cookie.value);
+		// 			});
+		// 		},
+		// 	},
+		// }
 	);
 }
